0eee847a5545036109041d8f490ec2919bfd8d0a
nikmatRMT <nikforart@gmail.com>
2026-07-01 01:55:32 -0700

    Switch back to production offline mode to allow native GPS and Pedometer sensors via localhost

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react()],
   server: {
     allowedHosts: true,
-    https: true,
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
