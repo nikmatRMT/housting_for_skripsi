de6348febc392bc6987555f69898e29cc11d24aa
nikmatRMT <nikforart@gmail.com>
2026-07-08 11:26:47 +0800

    fix: registration lifecycle

--- client/android/app/src/main/java/com/jasawarga/app/MainActivity.java
+++ client/android/app/src/main/java/com/jasawarga/app/MainActivity.java
@@ -6,8 +6,8 @@ import com.getcapacitor.BridgeActivity;
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        // Daftarkan plugin QuestPolling ke Capacitor Bridge
-        registerPlugin(QuestPollingPlugin.class);
         super.onCreate(savedInstanceState);
+        // Daftarkan plugin QuestPolling ke Capacitor Bridge setelah super.onCreate
+        registerPlugin(QuestPollingPlugin.class);
     }
 }
